cd95d91c7e257a31e32f14dc8522bb1a56938fd1
Luc de Wit <luc.dewit@hotmail.nl>
2023-03-02 15:11:07 +0100

    added links for oktabs

--- index.php
+++ index.php
@@ -3,14 +3,12 @@
 // Read and parse routes.json
 $routes = json_decode(file_get_contents('routes.json'), true);
 
-// Split the path on / and take the last one
-$path = $_SERVER['REQUEST_URI'];
-
-// Remove the first /
-$path = substr($path, 1);
+// Remove the first / of the request URI
+$path = substr($_SERVER['REQUEST_URI'], 1);
 
 // Get where the path leads to
 $destination = $routes[$path];
 
 // Redirect to the destination
 header('Location: ' . $destination);
+
