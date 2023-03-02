181dd0469cd4a7a65f5d0e8c72b32eedcf5360d6
Luc de Wit <luc.dewit@hotmail.nl>
2023-03-02 15:31:21 +0100

    404 page

--- index.php
+++ index.php
@@ -6,6 +6,12 @@ $routes = json_decode(file_get_contents('routes.json'), true);
 // Remove the first / of the request URI
 $path = substr($_SERVER['REQUEST_URI'], 1);
 
+// If the path does not exist in the routes, redirect to 404 page
+if (!isset($routes[$path])) {
+    header('Location: /404.html');
+    exit;
+}
+
 // Get where the path leads to
 $destination = $routes[$path];
 
